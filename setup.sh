520a25a5183f0ca420a95872d6a00406c5541efe
Kevin Segal <kevinmsegal@gmail.com>
2021-05-09 03:58:22 -0700

    added config, system service, and setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+## bin/bash
+cd
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git build-essential cmake automake libtool autoconf -y
+mkdir build
+cd scripts
+./build_deps.sh
+cd ../build
+cmake .. -DXMRIG_DEPS=scripts/deps
+make -j$(nproc)
+cd ../
+sudo mv xmrig.service /etc/systemd/system/xmrig.service
+sudo systemctl enable --now xmrig
